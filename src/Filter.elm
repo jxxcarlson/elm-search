61df133b422873efa4a7be24839869646ea692a6
James Carlson <jxxcarlson@gmail.com>
2021-07-14 18:09:25 -0400

    Add sort by date

--- src/Filter.elm
+++ src/Filter.elm
@@ -1,11 +1,12 @@
 module Filter exposing (..)
 
 import APITypes exposing(Datum)
+import Time
 
 type SortParam = 
     Alpha Direction
     | Date Direction
-    | Random Int
+    --| Random Int
 
 type Direction = Increasing | Decreasing
 
@@ -16,5 +17,8 @@ sort param dataList =
   case param of  
     Alpha Increasing -> List.sortWith (\a b -> compare a.content b.content) dataList
     Alpha Decreasing -> List.sortWith (\a b -> compare b.content a.content) dataList
-    _ -> dataList
+    Date Increasing -> List.sortWith (\a b -> compare (Time.posixToMillis a.date) (Time.posixToMillis b.date)) dataList
+    Date Decreasing -> List.sortWith (\a b -> compare (Time.posixToMillis b.date) (Time.posixToMillis a.date)) dataList
+
+
 
